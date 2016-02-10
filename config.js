93afbb1a1f5fbcac3e039df2e5738c64e7a605d7
Hammy Havoc <hammy@splitanatom.com>
2016-02-10 15:48:05 +0000

    Added Android app files.

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'integrated-movement-arts-android',
+		wp_ws_url : 'https://www.integratedmovementarts.com/wp-appkit-api/integrated-movement-arts-android',
+		theme : 'wpak-theme-bootstrap-master',
+		version : '1.0',
+		app_title : 'Integrated Movement Arts',
+		debug_mode : 'off',
+		auth_key : 'In7lbH4LXS?&J^: t7xz-O0%e9qnrHEBEo9nj*nE<$`&oTC:>M+|M;za/$r,UHEz',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
