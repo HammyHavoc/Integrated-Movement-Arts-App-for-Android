8a5a5302c813a0ac157a268a40f8af0c94b500f6
Hammy Havoc <hammy@splitanatom.com>
2016-02-10 17:35:49 +0000

    Changed version code and version number.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ define( function ( require ) {
 		app_slug : 'integrated-movement-arts-android',
 		wp_ws_url : 'https://www.integratedmovementarts.com/wp-appkit-api/integrated-movement-arts-android',
 		theme : 'wpak-theme-bootstrap-feature-user-login',
-		version : '1.0',
+		version : '1.1',
 		app_title : 'Integrated Movement Arts',
 		debug_mode : 'off',
 		auth_key : 'In7lbH4LXS?&J^: t7xz-O0%e9qnrHEBEo9nj*nE<$`&oTC:>M+|M;za/$r,UHEz',
